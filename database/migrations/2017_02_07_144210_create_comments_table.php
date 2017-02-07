8778697a0540c01262f2c5cbdd0857816100aff9
Juliane Blier <blier.juliane@gmail.com>
2017-02-07 15:50:10 +0100

    Ajout d'un controller et model Comment

---
+++ database/migrations/2017_02_07_144210_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('id_product');
+            $table->string('author');
+            $table->text('content');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
